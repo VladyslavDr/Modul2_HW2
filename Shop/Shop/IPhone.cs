09b42ad8f95b11fa2f84365431b68833bbe7bec4
Vladyslav Drushliak <vladislav.drushliak@gmail.com>
2022-09-14 15:35:02 +0300

    Added the product template

---
+++ Shop/Shop/IPhone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Shop
+{
+    public class IPhone
+    {
+        private IPhoneModels _model;
+        private int _cost;
+        private int _memory;
+        private ConsoleColor _color;
+
+        public IPhone(IPhoneModels model, int cost, int memory, ConsoleColor color)
+        {
+            _model = model;
+            _cost = cost;
+            _memory = memory;
+            _color = color;
+        }
+    }
+}
